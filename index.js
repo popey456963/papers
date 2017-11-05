f66283bdec3fa24b24ad2731d61db4cd3d5c332f
Alexander Craggs <popey@debenclipper.com>
2017-11-05 14:02:25 +0000

    Render

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const server = require('server')
 const { get, post } = server.router
+cosnt { render, json } = server.reply
 
 server({ port: 3000 }, {
 	get('/', ctx => 'Hello world')
+	get('/hi', ctx => render(''))
 })
