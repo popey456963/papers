ef7761bd3a99641c36ff526f3025a15eca1d0dfe
Alexander Craggs <popey@debenclipper.com>
2017-11-05 14:11:47 +0000

    Handlebars

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const exphbs  = require('express-handlebars')
 const express = require('express')
+
 const app = express()
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
 	res.send('Hello World!')
